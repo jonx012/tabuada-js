3054081307cadcc599e9998120a44fdf824c724b
João <72712307+jonx012@users.noreply.github.com>
2022-12-30 00:09:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function gerar() {
     var numEscolhido = document.querySelector("#numEscolhido")
     var nNumEscolhido = Number(numEscolhido.value)
     var tbd = document.querySelector("#tbd")
-
     if (numEscolhido.value.length == 0) {
         alert("Por favor, insira um número!")
     } else {
@@ -12,9 +11,7 @@ function gerar() {
             var res = nNumEscolhido * i
             var item = document.createElement("option")
             item.text = `${nNumEscolhido} x ${i} = ${res}`
-            item.value = `tab${c}`
             tbd.appendChild(item)
-            
             i++
         }
     }
